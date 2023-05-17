e417894309e61e3d420e886601319f5706059d5a
YaoYotw <10946028@ntub.edu.tw>
2023-05-17 17:27:36 +0800

    0514新增財金管一樓內部物件程式檔

---
+++ Unity/Script/BookDialog.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BookDialog : MonoBehaviour
+{
+    public Transform player;//主角
+    public GameObject dialogUI;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        dialogUI.GetComponentInChildren<Button>().onClick.AddListener(DialogClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")
+            && other.GetType().ToString() == "UnityEngine.CapsuleCollider")
+            {
+                Debug.Log("Playerenter");
+                dialogUI.SetActive(true);
+                
+                
+            }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")
+            && other.GetType().ToString() == "UnityEngine.CapsuleCollider")
+            {
+                Debug.Log("Playeexit");
+                dialogUI.SetActive(false);
+            
+            }
+    }
+
+
+    void DialogClick()
+    {
+        dialogUI.SetActive(false);
+        
+    }
+}

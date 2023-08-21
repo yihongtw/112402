23dbda8cdbfa510d8ee20c06d2192bfedb2c6a22
yihongtw <10946005@ntub.edu.tw>
2023-08-22 00:11:11 +0800

    0822 書籍類別留言板

---
+++ 網頁/0822PROJECT/login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登入</title>
+    
+</head>
+
+<body>
+
+    <?php
+    require_once('conn.php'); // 引入這些資料
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+
+    // 查詢資料庫到底有沒有這些登入的資料
+    // 使用 prepared statement 避免 SQL Injection
+    $sql = "SELECT * FROM registeruser WHERE user=? AND password=?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $user, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $_SESSION['username'] = $row['username'];
+
+        // 在這裡加入 JavaScript 代碼，彈出登入成功的提示框
+        echo '<script>';
+        echo 'alert("登入成功");';
+        echo 'window.location.href = "首頁.php";'; // 重定向到首頁
+        echo '</script>';
+    } else {
+        echo '登入失敗';
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ 網頁/0822PROJECT/register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>註冊</title>
+</head>
+<body>
+    <?php
+    require_once('conn.php');
+    $user = $_POST['user']; // 填入帳號密碼姓名
+    $password = $_POST['password'];
+    $username = $_POST['username'];
+
+    $sql = "SELECT `user` FROM `registeruser` WHERE `user`='$user'";
+    $result = $conn->query($sql);
+
+    if ($user === '' or $password === '') {
+        echo '帳號或密碼沒有填入';
+    } else if ($result->num_rows > 0) { // 大於零代表有這個資料，
+        echo '這個 user:' . $user . ' 已經註冊過'; {
+        }
+    } else {
+        $sql = "INSERT INTO `registeruser`(`user`, `password`,`username`) VALUES ('$user' ,'$password','$username')";
+        $conn->query($sql); // 這樣寫才會把資料推上去
+        echo '你的帳號以成功新增。username：' . $username;
+        echo '<meta http-equiv="refresh" content="2;url=index.php">';
+    }
+
+    ?>
+</body>
+
+</html>
